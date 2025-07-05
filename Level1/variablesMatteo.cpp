72218538564cd7f045d060e2b0e9f115427b6783
ferro68 <ferrarimatteo840@gmail.com>
2025-07-05 19:14:23 +0200

    data types and variables

---
+++ Level1/variablesMatteo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int temperature=20;
+    double price=3.50;
+    float pi=3.145;
+    string name="michele";
+
+    cout<<temperature<<"°C\n";
+    cout<<"€ "<<price<<"\n";
+    cout<<pi<<"\n";
+    cout<<"Ciao mi chiamo "<<name<<"\n";
+
+    return 0;
+
+}
+
+
+
