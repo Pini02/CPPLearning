8091bb1e846f327309ebde744439c05b4447d0db
ferro68 <ferrarimatteo840@gmail.com>
2025-07-21 17:57:07 +0200

    esercizio livello1 Matteo

---
+++ Level1/ex2Matte.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n1,n2,n3;
+    
+    cout<<"Dimmi due numeri\n";
+    cin>>n1;
+    cin>>n2;
+    n3= n1+n2;
+    cout<<n3;
+
+    return 0;
+}
+
